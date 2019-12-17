85dafc3f2c50fb7a68ab90d472b76a2946226f33
mahid <mo47da@gmail.com>
2019-12-17 12:48:32 +0600

    added the cache element for home page

--- index.js
+++ index.js
@@ -3,44 +3,49 @@ const RedditScraper = require("reddit-scraper");
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config(); //this is fro hiding secret in .env file
+const NodeCache = require("node-cache");
+const cache = new NodeCache({
+  stdTTL: 0
+});
+
 
 var allMeme = new Object();
-
+const rd45sdf = "site-home-stuff";
 app.set("view engine", "ejs");
 
+const redditScraperOptions = {
+  AppId: process.env.API_ID, // enter the id here
+  AppSecret: process.env.API_KEY // enter the secret here
+};
+
+const redditScraper = new RedditScraper.RedditScraper(redditScraperOptions);
+console.log("Configuration Loaded!");
+
+const scrapedata = async (SubReddit) => {
+  try {
+    class Memeobj {
+      constructor(SubReddit) {
+        this.Pages = 4;
+        this.Records = 25;
+        this.SortType = "top";
+        this.SubReddit = SubReddit;
+        this.Info = null;
+        this.urls = [];
+      }
+    }
+
+    const obj = new Memeobj(SubReddit);
+    obj.Info = await redditScraper.scrapeData(obj);
+    obj.urls = await obj.Info.map((obj) => obj.data.url);
+
+    return obj.urls;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 (async () => {
-  const redditScraperOptions = {
-    AppId: process.env.API_ID, // enter the id here
-    AppSecret: process.env.API_KEY // enter the secret here
-  };
-
   try {
-    const redditScraper = new RedditScraper.RedditScraper(redditScraperOptions);
-    console.log("Configuration Loaded!");
-
-    const scrapedata = async (SubReddit) => {
-      try {
-        class Memeobj {
-          constructor(SubReddit) {
-            this.Pages = 4;
-            this.Records = 25;
-            this.SortType = "top";
-            this.SubReddit = SubReddit;
-            this.Info = null;
-            this.urls = [];
-          }
-        }
-
-        const obj = new Memeobj(SubReddit);
-        obj.Info = await redditScraper.scrapeData(obj);
-        obj.urls = await obj.Info.map((obj) => obj.data.url);
-        return obj.urls;
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
     allMeme.memes = await scrapedata("memes");
     console.log("Memes Subreddit Scraped!");
 
@@ -53,27 +58,26 @@ app.set("view engine", "ejs");
     allMeme.MemeEconomy = await scrapedata("MemeEconomy");
     console.log("MemeEconomy Subreddit Scraped");
 
-    var scrapedData = [].concat.apply([], [...Object.values(allMeme)]);
-
-    console.log(scrapedData.length);
-
-    console.log(scrapedData.length + " total memes fetched.");
+    allMeme.all = [].concat.apply([], [...Object.values(allMeme)]);
+    allMeme.home = JSON.stringify(allMeme.all);
+    cache.set(rd45sdf, allMeme.home);
+    console.log(allMeme.all.length + " total memes fetched.");
   } catch (error) {
     console.log(error);
   }
-
-  app.get("/", function (req, res) {
-    res.render("index", {
-      url: scrapedData
-    });
-  });
-
-  app.get("/apis", (req, res) => {
-    res.json(scrapedData);
-  });
-
-  var port = process.env.PORT || 3000;
-  app.listen(port, function () {
-    console.log("server has started in " + port);
-  });
 })();
+
+
+app.get("/", function (req, res) {
+  const home = JSON.parse(cache.get(rd45sdf));
+  res.render("index", {
+    url: home
+  });
+});
+
+
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("server has started in " + port);
+});

5b2715ba573c5283b1e405ec7382865982613ebf
mahid <mo47da@gmail.com>
2019-12-21 14:10:15 +0600

    added client site caching

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }))
-
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'public,max-age=21600');
+});
 // app.use(express.static(__dirname + '/public'))
 
 var allMeme = new Object();

0057413d73b5ee32e3fe1fd2aeeab742d52c1553
mahid <mo47da@gmail.com>
2019-12-21 19:18:21 +0600

    tried to fix by removing next in line 18

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({
 
 app.use((req, res, next) => {
   res.set('Cache-Control', 'public,max-age=31600');
-  next();
+
 });
 // app.use(express.static(__dirname + '/public'))
 

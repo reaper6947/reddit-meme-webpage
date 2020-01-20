da92c168c6f52dd524d32420c9eefd93831a81a0
mahid <mo47da@gmail.com>
2020-01-20 23:18:44 +0600

    removed commented out code

--- index.js
+++ index.js
@@ -2,78 +2,56 @@
 const RedditScraper = require("reddit-scraper");
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv").config(); //this is fro hiding secret in .env file
+const dotenv = require("dotenv").config();
 const NodeCache = require("node-cache");
 const cache = new NodeCache({
-  stdTTL: 46400
+  stdTTL: 31600
 });
-
-app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }))
-
 app.use((req, res, next) => {
-  res.set('Cache-Control', 'public,max-age=31600');
+  res.set('Cache-Control', 'public,max-age=21600');
   next()
 })
-
-// app.use(express.static(__dirname + '/public'))
-
-// var allMeme = new Object();
 app.set("view engine", "ejs");
-
+app.use(express.json());
 const redditScraperOptions = {
   AppId: process.env.API_ID, // enter the id here
   AppSecret: process.env.API_KEY // enter the secret here
 };
-
+// app.use(express.static(__dirname + '/public'))
+// var allMeme = new Object();
 const redditScraper = new RedditScraper.RedditScraper(redditScraperOptions);
 console.log("Configuration Loaded!");
 
 const scrapedata = async (SubReddit) => {
   try {
     class Memeobj {
-      constructor(SubReddit) {
-        this.Pages = 5;
+      constructor(Sub) {
+        this.Pages = 4;
         this.Records = 25;
         this.SortType = "top";
-        this.SubReddit = SubReddit;
-        this.Info = null;
-        this.urls = [];
+        this.SubReddit = Sub;
+        /* this.Info = null; */
+        /* this.urls = []; */
       }
-    }
-
+    };
     const obj = new Memeobj(SubReddit);
-    obj.Info = await redditScraper.scrapeData(obj);
-    obj.urls = await obj.Info.map((obj) => obj.data.url);
-    var imgUrl = obj.urls.filter(name => name.includes('.jpg') || name.includes('.png') || name.includes('.jpeg') || name.includes('.gif'));
+    const memeData = await redditScraper.scrapeData(obj);
+    const memeUrls = await memeData.map(obj => obj.data.url);
+    const imgUrl = memeUrls.filter(name => name.includes('.jpg') || name.includes('.png') || name.includes('.jpeg') || name.includes('.gif'));
     return imgUrl;
   } catch (err) {
-    console.log("error in scrape");
+    console.log('error in scrape');
   }
 };
-/*
-async () => {
-  try {
-    allMeme.memes = await scrapedata("memes");
-    console.log("Memes Subreddit Scraped!");
-
-    // allMeme.all = [].concat.apply([], [...Object.values(allMeme)]);
-    // allMeme.home = JSON.stringify(allMeme.all);
-    cache.set("rd45sdf", allMeme.memes);
-    console.log(allMeme.memes.length + " total memes fetched.");
-  } catch (error) {
-    console.log(error);
-  }
-};
-*/
 
 app.get("/", async (req, res) => {
   try {
     if (cache.has("memes")) {
       console.log("memes is in cache")
-      content = await JSON.parse(cache.get("memes"));
+      let content = await JSON.parse(cache.get("memes"));
       return res.render("index", {
         url: content
       });
@@ -84,12 +62,10 @@ app.get("/", async (req, res) => {
     console.log("memes cached")
     res.render("index", {
       url: memes
-
     });
   } catch (err) {
     console.log("error in home scrape")
   }
-
 });
 
 const check = async (req, res, next) => {
@@ -111,39 +87,7 @@ const check = async (req, res, next) => {
     console.log('err in check')
   }
 };
-/*
-const set = async (req, res, next) => {
-  try {
-    const {
-      sub
-    } = req.params;
-    const Urls = await scrapedata(sub);
-    const urls = JSON.stringify(Urls);
-    console.log(Urls.length);
-    cache.set(sub, urls);
-    return res.render("index", {
-      url: Urls
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500);
-  }
-  next();
-};
 
-const get = (req, res, next) => {
-  const {
-    sub
-  } = req.params;
-  const content = cache.get(sub);
-  if (content) {
-    return res.render("index", {
-      url: JSON.parse(content)
-    });
-  }
-  return next();
-};
-*/
 app.get("/img/:sub", check, async (req, res) => {
   try {
     const {
@@ -168,6 +112,6 @@ app.post("/", (req, res, next) => {
 });
 
 var port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log("server has started in " + port);
 });

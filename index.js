385c972716c307a0bc59093c5e3e0fd4497168e9
mahid <mo47da@gmail.com>
2020-01-22 02:00:02 +0600

    add to home

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const scrapedata = async (SubReddit) => {
   try {
     class Memeobj {
       constructor(Sub) {
-        this.Pages = 4;
+        this.Pages = 1;
         this.Records = 25;
         this.SortType = "top";
         this.SubReddit = Sub;

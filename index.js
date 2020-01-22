e60c5fc49b7d20af4eb4e8512583effbec2be48b
mahid <mo47da@gmail.com>
2020-01-22 10:25:07 +0600

    increased meme number

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const scrapedata = async (SubReddit) => {
   try {
     class Memeobj {
       constructor(Sub) {
-        this.Pages = 1;
+        this.Pages = 4;
         this.Records = 25;
         this.SortType = "top";
         this.SubReddit = Sub;

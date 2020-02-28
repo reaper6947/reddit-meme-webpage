847182d676bfa7b298b78abf4549ecf1c8ba5350
mahid <mo47da@gmail.com>
2020-02-28 10:36:12 +0600

    added cache expire age

--- index.js
+++ index.js
@@ -4,33 +4,26 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
 const NodeCache = require("node-cache");
-const cache = new NodeCache({
-  stdTTL: 3600
-});
+const cache = new NodeCache({stdTTL: 3600});
 
-app.use(express.urlencoded({
-  extended: true
-}))
-/*
+app.use(express.urlencoded({extended: true}))
+
 app.use((req, res, next) => {
-  res.set('Cache-Control', 'public,max-age=21600');
+  res.set('Cache-Control', 'public,max-age=7200');
   next()
 })
-*/
+
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static(__dirname + '/views'));
 // var allMeme = new Object();
-
 
 const scrapedata = async (SubReddit) => {
   try {
     //const imgUrl = await axios.get(`https://reddit-zeit.now.sh/url/${SubReddit}`)
     const imgUrl = axios.get(`https://reddit-zeit.now.sh/url/${SubReddit}`).then(resp => {return(resp.data);});
     return imgUrl;
-  } catch (err) {
-    console.log(err);
-  }
+  } catch (err) {console.log(err);}
 };
 
 app.get("/", async (req, res) => {
@@ -38,34 +31,25 @@ app.get("/", async (req, res) => {
     if (cache.has("memes")) {
       console.log("memes is in cache")
       let content = await JSON.parse(cache.get("memes"));
-      return res.render("index", {
-        url: content
-      });
+      return res.render("index", {url: content});
     }
     const memes = await scrapedata("memes");
     console.log("homepage url scraped");
     cache.set("memes", JSON.stringify(memes));
     console.log("memes cached")
-    res.render("index", {
-      url: memes
-    });
+    res.render("index", {url: memes});
   } catch (err) {
-    console.log("error in home scrape")
+    console.log(`${err}`)
   }
 });
 
 const check = async (req, res, next) => {
   try {
-
-    const {
-      sub
-    } = req.params;
+    const {sub} = req.params;
     if (cache.has(sub)) {
       console.log(`${sub} is in cache`);
       const content = await JSON.parse(cache.get(sub));
-      return res.render("index", {
-        url: content
-      });
+      return res.render("index", {url: content});
     } else {
       return next();
     }
@@ -76,15 +60,11 @@ const check = async (req, res, next) => {
 
 app.get("/img/:sub", check, async (req, res) => {
   try {
-    const {
-      sub
-    } = req.params;
+    const {sub} = req.params;
     const content = await scrapedata(sub);
     const strContent = JSON.stringify(content);
     cache.set(sub, strContent);
-    return res.render("index", {
-      url: content
-    });
+    return res.render("index", {url: content});
   } catch (err) {
     console.log('error in get');
     return res.status(500);

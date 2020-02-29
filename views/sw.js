8a24e208d2fd904fe194fe36a9852699017b91b9
mahid <mo47da@gmail.com>
2020-02-29 19:06:38 +0600

    final version of sw worker

--- views/sw.js
+++ views/sw.js
@@ -38,101 +38,59 @@ async function networkFirst(request) {
 }
 */
 
-
-// This is the service worker with the Cache-first network
-
-const CACHE = "myCache";
-const precacheFiles = [
+const cacheName = 'meme911-v1';
+const staticAssets = [
   "/manifest.json",
   "/images/brand.svg",
   "/images/favicon.ico",
   "/index.ejs"
 ];
 
-self.addEventListener("install", function(event) {
-  console.log("[PWA Builder] Install Event processing");
-
-  console.log("[PWA Builder] Skip waiting on install");
-  self.skipWaiting();
-
-  event.waitUntil(
-    caches.open(CACHE).then(function(cache) {
-      console.log("[PWA Builder] Caching pages during install");
-      return cache.addAll(precacheFiles);
-    })
-  );
+self.addEventListener('install', async e => {
+  console.log("Install Event processing");
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
 });
 
-// Allow sw to control of current page
-self.addEventListener("activate", function(event) {
-  console.log("[PWA Builder] Claiming clients for current page");
+self.addEventListener('activate', e => {
+  console.log("Claiming clients for current page");
   var cacheKeeplist = [cacheName];
     event.waitUntil(
         caches.keys().then( keyList => {
             return Promise.all(keyList.map( key => {
-                if (cacheKeeplist.indexOf(key) === -1) {
-                    return caches.delete(key);
-                }
+                if (cacheKeeplist.indexOf(key) === -1) {return caches.delete(key);}
             }));
         })
 .then(self.clients.claim())); //this line is important in some contexts
 });
 
-// If any fetch fails, it will look for the request in the cache and serve it from there first
-self.addEventListener("fetch", function(event) {
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    fromCache(event.request).then(
-      function(response) {
-        // The response was found in the cache so we responde with it and update the entry
-
-        // This is where we call the server to get the newest version of the
-        // file to use the next time we show view
-        event.waitUntil(
-          fetch(event.request).then(function(response) {
-            return updateCache(event.request, response);
-          })
-        );
-
-        return response;
-      },
-      function() {
-        // The response was not found in the cache so we look for it on the server
-        return fetch(event.request)
-          .then(function(response) {
-            // If request was success, add or update it in the cache
-            event.waitUntil(updateCache(event.request, response.clone()));
-
-            return response;
-          })
-          .catch(function(error) {
-            console.log(
-              "[PWA Builder] Network request failed and no cache." + error
-            );
-          });
-      }
-    )
-  );
+self.addEventListener('fetch', async e => {
+  const req = e.request;
+  const url = new URL(req.url);
+  if (url.origin === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(networkAndCache(req));
+  }
 });
 
-function fromCache(request) {
-  // Check to see if you have it in the cache
-  // Return response
-  // If not in the cache, then return
-  return caches.open(CACHE).then(function(cache) {
-    return cache.match(request).then(function(matching) {
-      if (!matching || matching.status === 404) {
-        return Promise.reject("no-match");
-      }
-
-      return matching;
-    });
-  });
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
 }
 
-function updateCache(request, response) {
-  return caches.open(CACHE).then(function(cache) {
-    return cache.put(request, response);
-  });
+async function networkAndCache(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const fresh = await fetch(req);
+    await cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    const cached = await cache.match(req);
+    return cached;
+  }
 }
+
+

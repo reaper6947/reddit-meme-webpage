64b7f0e5edb5325fb491fd89d4a73df8042f322e
mahid <mo47da@gmail.com>
2020-03-08 11:35:17 +0600

    added lozad

--- views/sw.js
+++ views/sw.js
@@ -3,11 +3,11 @@ const staticAssets = [
   "/manifest.json",
   "/images/brand.svg",
   "/images/favicon.ico",
-    "/js/galleria.min.js",
-    "/js/themes/classic/galleria.classic.css",
-    "/js/themes/classic/galleria.classic.min.js",
-    "/js/bootstrap.min.js"
-    
+  "/js/galleria.min.js",
+  "/js/themes/classic/galleria.classic.css",
+  "/js/themes/classic/galleria.classic.min.js",
+  "/js/bootstrap.min.js",
+  "/js/lozad"
 ];
 
 self.addEventListener("install", async (e) => {
@@ -36,18 +36,17 @@ self.addEventListener("activate", (e) => {
   ); //this line is important in some contexts
 });
 
-self.addEventListener('fetch', async e => {
-    const req = e.request;
-    const url = new URL(req.url);
-    if (url.origin === location.origin) {
-      e.respondWith(cacheFirst(req));
-    } else {
-      e.respondWith(cacheFirst(req));
-    }
-  });
-  async function cacheFirst(req) {
-    const cache = await caches.open(cacheName);
-    const cached = await cache.match(req);
-    return cached || fetch(req) ;
+self.addEventListener("fetch", async (e) => {
+  const req = e.request;
+  const url = new URL(req.url);
+  if (url.origin === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(cacheFirst(req));
   }
-  
+});
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
+}
